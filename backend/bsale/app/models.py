c6e1889dc790230c794cdc42224af295c3897795
chriiisss17 <christianmontoya.cm18@gmail.com>
2022-04-06 17:57:13 -0400

    se cargó el models

--- backend/bsale/app/models.py
+++ backend/bsale/app/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+#Models
+class Category(models.Model):
+    name = models.CharField(max_length=255, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'category'
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255, blank=True, null=True)
+    url_image = models.CharField(max_length=255, blank=True, null=True)
+    price = models.FloatField(blank=True, null=True)
+    discount = models.IntegerField(blank=True, null=True)
+    category = models.ForeignKey(Category, models.DO_NOTHING, db_column='category', blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'product'
+
+    def __str__(self) -> str:
+        return self.name
