5415e9fe5aaaa8d949b1c7f8370382d45c3bee91
Christian Montoya <christian.montoya@bennu.cl>
2022-07-06 15:07:27 -0400

    feat: simbolo peso agregado al precio

--- frontend/js/scripts.js
+++ frontend/js/scripts.js
@@ -59,7 +59,7 @@ const getData = async(param) => {
             <img src="${element.url_image ? (element.url_image) : ('https://img.freepik.com/free-vector/internet-network-warning-404-error-page-file-found-web-page-internet-error-page-issue-found-network-404-error-present-by-man-sleep-display_1150-55450.jpg?w=1380&t=st=1649390433~exp=1649391033~hmac=3111cb6e39431bdd2f8954e9bf07f57c2cfdfec0c82a6956d06e99461119e75e')}" class="card-img-top" alt="..." style="width: 150px;height:150px;">
                 <div class="card-body">
                     <h5>${element.name}</h5>
-                    <p class="card-text">${element.price}</p>
+                    <p class="card-text">$${element.price}</p>
                 </div>
             </div>
             </div>
